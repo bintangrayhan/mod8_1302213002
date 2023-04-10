c5b17c990011093121e3ab0394754ac1dd9b2821
bintangrayhan <bintangrayhan7@gmail.com>
2023-04-10 08:16:24 +0700

    commit terakhir

---
+++ mod8_1302213022/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace mod8_1302213022
+{
+    internal class Class1
+    {
+        static void Main()
+        {
+            string uang = Console.ReadLine();
+
+            string json = "{}";
+            try
+            {
+                json = File.ReadAllText(@"D:\smester 4\kontruksi peerangkat lunak\prima ganteng\mod8_1302213022\mod8_1302213022\bank_tranfer_config.json");
+            }
+            catch(FileNotFoundException)
+            { 
+                
+            }
+            config config = JsonConverter.DeserializeObject<config>(json);
+            if (config.enggres == "en")
+            {
+                 Console.WriteLine ("please insert the amount of money");
+                 
+                 Console.WriteLine(uang);
+            }
+            else if (config.indo == "id")
+            {
+                Console.WriteLine  ("masukan uang anda");
+                
+                Console.WriteLine(uang);
+            }
+            if (uang <= config.low_fee ||uang == config.)
+
+
+        }
+    }
+}
---
+++ mod8_1302213022/config.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace mod8_1302213022
+{
+    internal class config
+    {
+        public int treshold { get; set;} = 25000000;
+        public int low_fee { get; set; } = 6500;
+        public int hight_fee { get; set; } = 1500;
+        public string enggres { get; set; } = "en";
+        public string indo { get; set; } = "id";
+
+        
+
+    }
+}
